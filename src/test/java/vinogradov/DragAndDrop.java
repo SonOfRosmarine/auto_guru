97347dd2dbff671e3547f7d2738c7d5bb0b03ae3
Roman Belov <roman.b1990@yandex.ru>
2023-03-02 21:18:29 +0300

    ReworckHomeWorck

--- src/test/java/vinogradov/DragAndDrop.java
+++ src/test/java/vinogradov/DragAndDrop.java
@@ -7,25 +7,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.sleep;
-import static java.nio.channels.SocketChannel.open;
 
-public class dragAndDrop {
+public class DragAndDrop {
 
     @BeforeAll
-    static void beforAll() {
+    static void beforeAll() {
 
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://the-internet.herokuapp.com";
     }
-@Test
 
-    void checkDragAndDrop(){
+    @Test
+    void checkDragAndDrop() {
 
-    Selenide.open("/drag_and_drop");
-    $("#column-a").shouldHave(Condition.exactText("A"));
-    $("#column-b").dragAndDropTo("#column-a");
-    $("#column-a").shouldHave(Condition.exactText("B"));
-}
+        Selenide.open("/drag_and_drop");
+        $("#column-a").shouldHave(Condition.exactText("A"));
+        $("#column-b").dragAndDropTo("#column-a");
+        $("#column-a").shouldHave(Condition.exactText("B"));
+    }
 
 }
--- src/test/java/vinogradov/EnterprizePage.java
+++ src/test/java/vinogradov/EnterprizePage.java
@@ -1,7 +1,6 @@
 package vinogradov;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -9,24 +8,22 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class enterprizePage {
+public class EnterprizePage {
 
 
     @BeforeAll
-    static void beforAll(){
+    static void beforeAll() {
 
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com/";
     }
 
-      @Test
-
-
-    void enterprizePage(){
-         open("https://github.com/");
+    @Test
+    void enterprisePage() {
+        open("https://github.com/");
         $(".header-menu-wrapper").$(byText("Solutions")).hover();
-        $$(".HeaderMenu-dropdown-link").findBy(text ("Enterprise")).click();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
         $(".enterprise-hero").shouldHave(text("Build like the best"));
-}
+    }
 
 }
