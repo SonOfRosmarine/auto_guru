1c4ffae2b697d2be98fd6c520283cfe5c9c351ea
Roman Belov <roman.b1990@yandex.ru>
2023-03-30 19:06:47 +0300

    HomeWorkTestBase3

--- src/test/java/form/CheckForm.java
+++ src/test/java/form/CheckForm.java
@@ -1,13 +1,14 @@
 package form;
 
 import form.generate.GenerateRandomDate;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
 
 public class CheckForm extends TestBase {
 
-
+    @Tag("CheckForm")
     @Test
     public void successfulTest() {
         String userName = GenerateRandomDate.getRandomName();
@@ -24,34 +25,39 @@ public class CheckForm extends TestBase {
         String state = GenerateRandomDate.getRandomState();
         String city = GenerateRandomDate.getRandomCity(state);
 
-       step("Открыть страницу",() ->{ objectPageCheckForm.openPage();});
-        step( "Заполнить форму",() ->{
-                objectPageCheckForm .secretBanner()
-                .setInputFirstName(userName)
-                .setInputLastName(lastName)
-                .setInputEmail(email)
-                .setInputGender(gender)
-                .setInputNumber(number)
-                .setBirthMonth(month)
-                .setBirthYears(years)
-                .setBirthDay(day)
-                .setInputSubject(subjects)
-                .setHobbies(hobbies)
-                .setUploadPicture()
-                .setAddress(address)
-                .setState(state)
-                .setCity(city)
-                .resultText();});
+        step("Открыть страницу", () -> {
+            objectPageCheckForm.openPage();
+        });
+        step("Заполнить форму", () -> {
+            objectPageCheckForm.secretBanner()
+                    .setInputFirstName(userName)
+                    .setInputLastName(lastName)
+                    .setInputEmail(email)
+                    .setInputGender(gender)
+                    .setInputNumber(number)
+                    .setBirthMonth(month)
+                    .setBirthYears(years)
+                    .setBirthDay(day)
+                    .setInputSubject(subjects)
+                    .setHobbies(hobbies)
+                    .setUploadPicture()
+                    .setAddress(address)
+                    .setState(state)
+                    .setCity(city)
+                    .resultText();
+        });
 
-             step("Осуществить проверку",() ->{  objectPageCheckForm .verifyResult("Student Name", userName + " " + lastName)
-                .verifyResult("Student Email", email)
-                .verifyResult("Gender", gender)
-                .verifyResult("Mobile", number)
-                .verifyResult("Date of Birth", day + " " + month + "," + years)
-                .verifyResult("Subjects", subjects)
-                .verifyResult("Hobbies", hobbies)
-                .verifyResult("Address", address)
-                .verifyResult("State and City", state + " " + city);});
+        step("Осуществить проверку", () -> {
+            objectPageCheckForm.verifyResult("Student Name", userName + " " + lastName)
+                    .verifyResult("Student Email", email)
+                    .verifyResult("Gender", gender)
+                    .verifyResult("Mobile", number)
+                    .verifyResult("Date of Birth", day + " " + month + "," + years)
+                    .verifyResult("Subjects", subjects)
+                    .verifyResult("Hobbies", hobbies)
+                    .verifyResult("Address", address)
+                    .verifyResult("State and City", state + " " + city);
+        });
     }
 
 
