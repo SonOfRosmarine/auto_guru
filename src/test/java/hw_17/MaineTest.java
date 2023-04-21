ceeb98fc884ed452a95131c278dd23df4009ba7f
Roman Belov <roman.b1990@yandex.ru>
2023-04-21 17:25:58 +0300

    HomeWorkTestBaseParamProp17

---
+++ src/test/java/hw_17/MaineTest.java
@@ -0,0 +1,46 @@
+package hw_17;
+
+import com.codeborne.selenide.Condition;
+import config.WebConfig;
+import io.qameta.allure.Step;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class MaineTest extends BaseTest {
+    private static final WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
+    private static final String REPOSITIRY = "SonOfRosmarine/auto_guru";
+    @Test
+    public void testIssueSearchLanbda() {
+        step("Открываем главную страницу", () -> {
+            open(webConfig.getBaseUrl());
+        });
+        step("Осуществляем поиск репозитория" + REPOSITIRY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITIRY);
+            $(".header-search-input").submit();
+        });
+        step("Кликаем по ссылке репазитория" + REPOSITIRY, () -> {
+            $(linkText(REPOSITIRY)).click();
+        });
+        step("Открываем таб issue", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверяем наличие записи", () -> {
+            $("#issue_1_link").shouldHave(text("Allure"));
+        });
+
+    }
+
+    @Step("Проверяем наличие Issue с хэштегом 904")
+    public void checkHashtag() {
+        $(withText("#904")).should(Condition.exist);
+    }
+}
+
